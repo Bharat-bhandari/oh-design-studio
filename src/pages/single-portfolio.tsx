4647979a4f55328f1322d42bba34721dca56e17d
Bharat Bhandari <bharatbhandari0302@gmail.com>
2024-04-27 22:21:40 +0530

    mobile portfolio also created

--- src/pages/single-portfolio.tsx
+++ src/pages/single-portfolio.tsx
@@ -1,12 +1,18 @@
 import Nav from "@/components/LandingPage/Nav";
+import SinglePortFolioDesignMobile from "@/components/MobileView/SinglePortFolioDesignMobile";
 import SinglePortFolioDesign from "@/components/PortfolioPage/SinglePortFolioDesign";
 import React from "react";
 
 const SinglePortfolio = () => {
   return (
     <>
-      <Nav />
-      <SinglePortFolioDesign />
+      <div className="hidden sm:block">
+        <Nav />
+        <SinglePortFolioDesign />
+      </div>
+      <div>
+        <SinglePortFolioDesignMobile />
+      </div>
     </>
   );
 };
